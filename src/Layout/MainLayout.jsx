72ba51d871dc41907145cf7081a4ff2fd563a262
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-11 16:02:18 +0600

    added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
-// import Footer from '../Components/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
